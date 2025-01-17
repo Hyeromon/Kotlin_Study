7b40e1170813a977ebdfb9df57bdfc41df3ade84
Hyeromon <trina9018@gmail.com>
2025-01-17 10:22:25 +0900

    [day4]추상클래스/ 추상메서드

--- AbstractEx01.kt
+++ AbstractEx01.kt
@@ -1,13 +1,29 @@
-class Animal(){
+//추상클래스
+abstract class Animal(){
     init{
         println("Animal 초기화 블럭")
     }
     fun bark(){
         println("멍멍")
     }
+    //추상메서드
+    abstract fun eat()
+}
+
+class Dog: Animal(){
+    init{
+        println("Dog 초기화 블럭")
+    }
+    override fun eat(){
+        println("사료를 먹는다")
+    }
 }
 
 fun main(){
-    var a: Animal = Animal()
-    a.bark()
+    // var a: Animal = Animal()
+    // a.bark()
+
+    var d: Dog = Dog()
+    d.bark()
+    d.eat()
 }
