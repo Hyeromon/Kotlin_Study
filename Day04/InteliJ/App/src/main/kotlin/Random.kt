7428dce419f5a84ef8f157c527ffc7ca1e1e1256
Hyeromon <trina9018@gmail.com>
2025-01-17 15:49:08 +0900

    [day4]

--- Day04/InteliJ/App/src/main/kotlin/Random.kt
+++ Day04/InteliJ/App/src/main/kotlin/Random.kt
@@ -6,7 +6,9 @@ fun main(){
     var list = List(10){Random.nextInt(0,100)}
     println("0-100까지 번호 랜덤 추첨 : ${list}")
 
-
-    var lotto = List(6){Random.nextInt(1,45)}
-    println("lotto번호 추첨 : ${lotto}")
+    for(i in 0 .. 5) {
+        var lotto = List(6) { Random.nextInt(1, 46) }
+        println("lotto번호 추첨 : ${lotto}")
+    }
 }
+
